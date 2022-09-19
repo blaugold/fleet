3588da91aac6dbdf47b1cbff8dfe66afeb680524
Gabriel Terwesten <gabriel@terwesten.net>
2022-09-19 13:25:50 +0200

    migrate to new sync transactions

--- packages/fleet_view/lib/fleet_view.dart
+++ packages/fleet_view/lib/fleet_view.dart
@@ -1,4 +1,3 @@
-export 'src/animate.dart' show withAnimation;
 export 'src/annotation.dart' show view, state;
 export 'src/state.dart' show StateSetter, updateState, SetStateReason;
 export 'src/view.dart' show FleetView, FleetViewWidget, ViewElement;
--- packages/fleet_view/lib/src/animate.dart
+++
@@ -1,8 +0,0 @@
-import 'package:fleet/fleet.dart';
-
-import 'state.dart';
-
-/// Applies an [animation] to the state changes caused by calling [block].
-T withAnimation<T>(AnimationSpec? animation, T Function() block) {
-  return withTransaction(animation, block);
-}
