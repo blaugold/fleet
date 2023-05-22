a30fd11386e1b6c7b07606036fa32df4d7779644
Gabriel Terwesten <gabriel@terwesten.net>
2023-05-22 20:53:25 +0200

    Refactor stack example

--- packages/fleet/example/lib/stack.dart
+++ packages/fleet/example/lib/stack.dart
@@ -5,38 +5,28 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: MyHomePage(),
-    );
-  }
+  State<MyApp> createState() => _MyAppState();
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
+class _MyAppState extends State<MyApp> {
   var _left = true;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: GestureDetector(
-        behavior: HitTestBehavior.opaque,
-        onTap: () => setState(() => _left = !_left),
-        child: Stack(
-          children: List.generate(
-            6,
-            (index) => StackElement(index: index, left: _left),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: GestureDetector(
+          behavior: HitTestBehavior.opaque,
+          onTap: () => setState(() => _left = !_left),
+          child: Stack(
+            children: [
+              for (var i = 0; i < 6; i++) StackElement(index: i, left: _left)
+            ],
           ),
         ),
       ),
@@ -45,30 +35,36 @@ class _MyHomePageState extends State<MyHomePage> {
 }
 
 class StackElement extends StatelessWidget {
-  const StackElement({super.key, required this.index, required this.left});
+  const StackElement({
+    super.key,
+    required this.index,
+    required this.left,
+  });
+
+  static const _stackDimension = 500.0;
+  static const _step = 70.0;
 
   final int index;
   final bool left;
 
-  AnimationSpec _buildAnimation() {
-    return AnimationSpec.curve(Curves.easeInOutCubic, 500.ms - 25.ms * index)
-        .delay(50.ms * index);
-  }
+  AnimationSpec _buildAnimation() =>
+      AnimationSpec.curve(Curves.easeInOutCubic, 500.ms - 25.ms * index)
+          .delay(50.ms * index);
 
   Color _buildColor() =>
       HSLColor.fromAHSL(1, ((index * 12) + 155) % 360.0, 1, .5).toColor();
 
   @override
   Widget build(BuildContext context) {
-    final dimension = 500.0 - (index * 70);
+    final elementDimension = _stackDimension - (index * _step);
     return Material(
       elevation: 50,
       color: _buildColor(),
-      borderRadius: BorderRadius.circular(dimension / 20),
+      borderRadius: BorderRadius.circular(elementDimension / 20),
     )
-        .square(dimension)
+        .square(elementDimension)
         .center()
-        .square(500)
+        .square(_stackDimension)
         .align(Alignment(left ? -.5 : .5, 0))
         .animation(_buildAnimation(), value: left);
   }
